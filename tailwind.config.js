a49da08cf672d3a5289bab3aa51cfd87309c50ee
Kojo Kwakye <kodeon6@gmail.com>
2024-04-12 15:56:06 +0100

    Initial commit2

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require('flowbite/plugin')
+    ],
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ]
+}
+ 
